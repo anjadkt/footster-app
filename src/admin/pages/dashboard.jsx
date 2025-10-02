5492fcbe55b94542c200decdd8891383a8e86942
anjadkt <anjad076@gmail.com>
2025-10-02 12:30:25 +0530

    clear

--- src/admin/pages/dashboard.jsx
+++ src/admin/pages/dashboard.jsx
@@ -2,9 +2,11 @@ import { useEffect, useState } from "react"
 import SideBar from "../components/sidebar"
 import '../styles/dashboard.css'
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 export default function Dashboard(){
   const [data,setData] = useState({});
+  const navigate = useNavigate();
 
   async function takeData() {
     const products = await axios.get('http://localhost:5000/products');
@@ -18,10 +20,22 @@ export default function Dashboard(){
       return orderCount;
     }
 
+    const calRevenue = ()=>{
+      let revenue = 0;
+      users.data.forEach((v)=>{
+        v.orders.forEach((o)=>{
+          console.log(o);
+          revenue += o.total
+        })
+      });
+      return revenue;
+    }
+
     setData({ 
       products : products.data.length,
       userCount : users?.data.length - 1,
-      orders : calOrders()
+      orders : calOrders(),
+      revenue : calRevenue()
     });
 
   }
@@ -30,7 +44,6 @@ export default function Dashboard(){
     document.title = "Admin panel"
   },[]);
   
-  console.log(data)
   return(
     <>
      <SideBar />
@@ -39,20 +52,35 @@ export default function Dashboard(){
       <hr />
       <div className="dashboard-details-container">
         <div className="dashboard-cus-or-pro-container">
-          <div>
+          <div onClick={()=>navigate('/users')}>
             <img src="./icons/users.png" alt="users" />
             <div>Customers</div>
             <h2>{ data.userCount}</h2>
           </div>
-          <div>
+          <div onClick={()=>navigate('/adminOrders')}>
             <img src="./icons/orders.png" alt="orders" />
             <div>Orders</div>
             <h2>{data.orders}</h2>
           </div>
-          <div>
+          <div onClick={()=>navigate('/allproducts')}>
             <img src="./icons/products.png" alt="products" />
             <div>Products</div>
             <h2>{ data.products}</h2>
+          </div>
+          <div>
+            <img src="./icons/products.png" alt="users" />
+            <div>Total income</div>
+            <h2 style={{color : 'green'}}>&#8377;{data.revenue}</h2>
+          </div>
+          <div>
+            <img src="./icons/products.png" alt="users" />
+            <div>Total expense</div>
+            <h2 style={{color : 'brown'}}>&#8377;{data.revenue*0.6}</h2>
+          </div>
+          <div>
+            <img src="./icons/products.png" alt="users" />
+            <div>Total earnings</div>
+            <h2 style={{color : 'green'}}>&#8377;{data.revenue*0.4}</h2>
           </div>
         </div>
         <div></div>
