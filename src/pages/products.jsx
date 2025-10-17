4f3aadaa2876ed6133da7a3e9506c80f082cc8e1
anjadkt <anjad076@gmail.com>
2025-10-17 20:33:56 +0530

    category added

--- src/pages/products.jsx
+++ src/pages/products.jsx
@@ -1,27 +1,42 @@
 import '../styles/products.css'
 import Header from "../components/header"
-import { useFetch } from "../customHooks/customHooks"
+import { useFetch} from "../customHooks/customHooks"
 import Product from "../components/product"
 import Title from '../components/title'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 
 export default function Products(){
-  const [products] = useFetch('http://localhost:5000/products');
+  const [products,setProducts] = useState([]);
+  const [cat,setCat] = useState("All");
+  async function setCategory(cate){
+    if(cate == 'All'){
+      const {data} = await axios('http://localhost:5000/products');
+      setProducts(data);
+      return ;
+    }
+    const {data} = await axios(`http://localhost:5000/products?category=${cat}`);
+    setProducts(data)
+  }
+  useEffect(()=>{
+    setCategory(cat);
+  },[cat])
   return(
     <>
      <Header />
      <main>
       <div className='category-selectors'>
-        <button>All</button>
-        <button>casuals</button>
-        <button>sports</button>
-        <button>indoor</button>
-        <button>loafers</button>
-        <button>boots</button>
-        <button>sandels</button>
-        <button>sneakers</button>
+        <button onClick={()=>setCat("All")}>All</button>
+        <button onClick={()=>setCat("casuals")}>casuals</button>
+        <button onClick={()=>setCat("sports")}>sports</button>
+        <button onClick={()=>setCat("indoor")}>indoor</button>
+        <button onClick={()=>setCat("loafers")}>loafers</button>
+        <button onClick={()=>setCat("boots")}>boots</button>
+        <button onClick={()=>setCat("sandels")}>sandels</button>
+        <button onClick={()=>setCat("sneakers")}>sneakers</button>
       </div>
       <div className='all-div'>
-        <Title title={"All products"}/>
+        <Title title={cat=="All" ? "All Products" : `${cat}`}/>
         <hr />
         <div className="all-products-container-div">
           { products &&
